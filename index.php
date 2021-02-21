5363bcbd1b8085ca02ad0d946945983fafd92dc7
Divyam Kumar <divyamkumarsingh@gmail.com>
2021-02-21 15:31:58 +0530

    pdfjs minimal implementation

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <script src="https://cdn.jsdelivr.net/npm/pdfjs-dist@2.0.943/build/pdf.min.js"></script>
+    <script src="index.js"></script>
+</head>
+<body>
+<!--<canvas id="my_canvas"></canvas>-->
+<script>
+
+    /**
+     * This is a minimal implementation of pdfjs
+     * The newer versions require pdf worker options
+     * @type {number}
+     */
+
+// pdfjsLib.getDocument('./pdf.pdf').then(doc => {
+//     console.log(doc._pdfInfo.numPages);
+//
+//     doc.getPage(2).then(page => {
+//         let myCanvas = document.getElementById("my_canvas");
+//         let context = myCanvas.getContext('2d');
+//
+//         let viewPort = page.getViewport(2);
+//         myCanvas.width = viewPort.width;
+//         myCanvas.height = viewPort.height;
+//
+//         page.render({
+//             canvasContext:context,
+//             viewport: viewPort
+//         });
+//
+//     });
+// });
+
+
+//=======================================================
+
+
+
+
+
+
+
+
+
+
+
+</script>
+</body>
+</html>
